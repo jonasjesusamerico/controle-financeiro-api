8db27cf505bb92036999ed9bd6ea00bbf857f5dd
Jonas de Jesus Américo <jjsamerico@gmail.com>
2022-12-03 16:37:35 -0300

    Ajustando local da criacao das rotas

---
+++ internal/router/routes.go
@@ -0,0 +1,28 @@
+package router
+
+import (
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joninhasamerico/controle-financeiro-api/internal/controllers"
+	"github.com/joninhasamerico/controle-financeiro-api/internal/router/middlewares"
+	"gorm.io/gorm"
+)
+
+func NewSetupRouter(engine *gin.Engine, dbCtx *gorm.DB) {
+	timeoutCtx := time.Duration(2) * time.Second
+
+	/* Middlewares */
+	engine.Use(middlewares.CORSMiddleware())
+
+	/* Groups */
+	MAIN_GROUP := engine.Group("/")
+	API_GROUP := MAIN_GROUP.Group("/api", middlewares.MiddleAuth(), middlewares.FilterTenantMiddleware())
+	V1_GROUP := API_GROUP.Group("/v1")
+
+	/* Login */
+	controllers.NewLoginController(MAIN_GROUP, dbCtx)
+
+	/* Usuario */
+	controllers.NewUsuarioController(MAIN_GROUP, V1_GROUP, dbCtx, timeoutCtx)
+}
